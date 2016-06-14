db853b4d0576cb902fb8780a2c604e691b1dd76e
U-AD\hali0031 <hali0031@ITS-PD0368.ad.umu.se>
2016-06-14 16:59:46 +0200

    Added manifest and echo api

--- src/main/java/eu/erasmuswithoutpaper/boundary/ConnectorResource.java
+++ src/main/java/eu/erasmuswithoutpaper/boundary/ConnectorResource.java
@@ -1,8 +1,19 @@
 package eu.erasmuswithoutpaper.boundary;
 
+
+import eu.erasmuswithoutpaper.api.echo.Response;
+import eu.erasmuswithoutpaper.api.echo.Vars;
+import java.io.IOException;
+import java.net.URISyntaxException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 import javax.ejb.Stateless;
+import javax.ws.rs.Consumes;
 import javax.ws.rs.GET;
+import javax.ws.rs.POST;
 import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
 
 @Stateless
 @Path("")
@@ -12,5 +23,25 @@ public class ConnectorResource {
   @Path("status")
   public String status() {
     return "I'm working!";
+  }
+
+  @GET
+  @Path("manifest")
+  @Produces(MediaType.APPLICATION_XML)
+  public String manifest() throws URISyntaxException, IOException  {
+          String content = new String(Files.readAllBytes(Paths.get(getClass().getClassLoader()
+                        .getResource("manifest.xml")
+                        .toURI())));
+          return content;
+  }
+  
+  @POST
+  @Path("echo")
+  @Consumes({MediaType.APPLICATION_XML, MediaType.APPLICATION_JSON})
+  @Produces(MediaType.APPLICATION_XML)
+  public Response echo(Vars vars) {
+      Response response = new Response();
+      response.setVars(vars);
+      return response;
   }
 }
