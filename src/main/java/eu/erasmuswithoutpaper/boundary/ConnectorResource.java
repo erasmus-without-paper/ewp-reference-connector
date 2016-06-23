98eaea5886184d8151a3c85736b454abe9f799a8
Håkan Lindgren <hakan.lindgren@umu.se>
2016-06-23 13:43:59 +0200

    Changed echo API to match new changes in branch stable-v1-review.

--- src/main/java/eu/erasmuswithoutpaper/boundary/ConnectorResource.java
+++ src/main/java/eu/erasmuswithoutpaper/boundary/ConnectorResource.java
@@ -2,13 +2,13 @@ package eu.erasmuswithoutpaper.boundary;
 
 
 import eu.erasmuswithoutpaper.api.echo.Response;
-import eu.erasmuswithoutpaper.api.echo.Vars;
 import java.io.IOException;
 import java.net.URISyntaxException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
+import java.util.List;
 import javax.ejb.Stateless;
-import javax.ws.rs.Consumes;
+import javax.ws.rs.FormParam;
 import javax.ws.rs.GET;
 import javax.ws.rs.POST;
 import javax.ws.rs.Path;
@@ -38,20 +38,19 @@ public class ConnectorResource {
   
   @POST
   @Path("echo")
-  @Consumes(MediaType.APPLICATION_XML)
   @Produces(MediaType.APPLICATION_XML)
-  public Response echo(Vars vars) {
+  public Response echoPost(@FormParam("echo") List<String> echo) {
       Response response = new Response();
-      response.setVars(vars);
+      echo.stream().forEach(e -> response.getEcho().add(e));
       return response;
   }
-
+ 
   @GET
   @Path("echo")
   @Produces(MediaType.APPLICATION_XML)
-  public Response echoGet(@QueryParam("vars") Vars vars) {
+  public Response echoGetSimple(@QueryParam("echo") List<String> echo) {
       Response response = new Response();
-      response.setVars(vars);
+      echo.stream().forEach(e -> response.getEcho().add(e));
       return response;
   }
 }
