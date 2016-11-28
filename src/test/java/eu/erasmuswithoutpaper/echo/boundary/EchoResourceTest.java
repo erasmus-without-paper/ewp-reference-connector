92a71be3e7f3db45055bde9a2f5aa22442e19e0b
Thom Jönsson <thom.jonsson@umu.se>
2016-11-28 15:48:20 +0100

    Minor bug fix.

--- src/test/java/eu/erasmuswithoutpaper/echo/boundary/EchoResourceTest.java
+++ src/test/java/eu/erasmuswithoutpaper/echo/boundary/EchoResourceTest.java
@@ -1,11 +1,11 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package eu.erasmuswithoutpaper.echo.boundary;
 
 import eu.erasmuswithoutpaper.api.echo.Response;
+import eu.erasmuswithoutpaper.organization.entity.Institution;
+import java.util.ArrayList;
+import java.util.List;
+import javax.persistence.EntityManager;
+import javax.persistence.Query;
 import javax.ws.rs.client.Entity;
 import javax.ws.rs.core.Application;
 import javax.ws.rs.core.MultivaluedHashMap;
@@ -14,16 +14,24 @@ import org.glassfish.jersey.server.ResourceConfig;
 import org.glassfish.jersey.test.JerseyTest;
 import static org.junit.Assert.*;
 import org.junit.Test;
+import org.mockito.Matchers;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
 
 public class EchoResourceTest extends JerseyTest {
 
+    private EchoResource resource;
+    
     @Override
     protected Application configure() {
-        return new ResourceConfig(EchoResource.class);
+        resource = new EchoResource();
+        resource.em = mock(EntityManager.class);
+        return new ResourceConfig().register(resource);
     }
 
     @Test
     public void testEchoGet() {
+        mockInstitutions();
         final Response echoResponse = target("echo")
                 .queryParam("echo", "Test1")
                 .queryParam("echo", "Test2")
@@ -34,6 +42,7 @@ public class EchoResourceTest extends JerseyTest {
         assertEquals(2, echoResponse.getEcho().size());
         assertTrue(echoResponse.getEcho().contains("Test1"));
         assertTrue(echoResponse.getEcho().contains("Test2"));
+        assertTrue(echoResponse.getHeiId().contains("myInstId"));
     }
 
     @Test
@@ -41,7 +50,9 @@ public class EchoResourceTest extends JerseyTest {
         MultivaluedMap<String, String> data = new MultivaluedHashMap<>();
         data.add("echo", "Test1");
         data.add("echo", "Test2");
-
+        
+        mockInstitutions();
+                
         final Response echoResponse = target("echo")
                 .request()
                 .post(Entity.form(data), Response.class);
@@ -50,6 +61,21 @@ public class EchoResourceTest extends JerseyTest {
         assertEquals(2, echoResponse.getEcho().size());
         assertTrue(echoResponse.getEcho().contains("Test1"));
         assertTrue(echoResponse.getEcho().contains("Test2"));
+        assertTrue(echoResponse.getHeiId().contains("myInstId"));
     }
 
+    void mockInstitutions() {
+        List<Institution> institutions = new ArrayList<>();
+        
+        Institution institution = new Institution();
+        institution.setInstitutionId("myInstId");
+        
+        institutions.add(institution);
+        mockQuery(institutions);
+    }
+    void mockQuery(List<Institution> results) {
+        Query mockedQuery = mock(Query.class);
+        when(mockedQuery.getResultList()).thenReturn(results);
+        when(this.resource.em.createNamedQuery(Matchers.anyString())).thenReturn(mockedQuery);
+    }
 }
